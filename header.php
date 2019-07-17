6eebe033304db4e8a6c9d11277e1e4d03858e6c6
Andrei Babor <andreibabor@yahoo.com>
2019-07-17 14:38:46 +0300

    header updates

--- header.php
+++ header.php
@@ -20,63 +20,63 @@
 
 <head>
 
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
 
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-
-
-<!-- favicon -->
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 
 
-<?php if ( of_get_option( 'custom_favicon' ) ) { ?>
-
-<link rel="icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" />
-
-<?php } ?>
+	<!-- favicon -->
 
 
 
-<!--[if IE]><?php if ( of_get_option( 'custom_favicon' ) ) { ?><link rel="shortcut icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" /><?php } ?><![endif]-->
+	<?php if ( of_get_option( 'custom_favicon' ) ) { ?>
+
+	<link rel="icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" />
+
+	<?php } ?>
 
 
 
-<?php wp_head(); ?>
-<style>
-.pp {
-    background-color: #1b7e7b;
-    border: 0 solid;
-    bottom: 0;
-    color: #fff;
-    float: right;
-    font-size: 12px;
-    font-weight: bold;
-    height: 21px;
-    margin-top: 0px;
-    padding-left: 6px;
-    padding-right: 6px;
-    position: relative;
-    right: 250px;
-}
+	<!--[if IE]><?php if ( of_get_option( 'custom_favicon' ) ) { ?><link rel="shortcut icon" href="<?php echo of_get_option( 'custom_favicon' ); ?>" /><?php } ?><![endif]-->
 
-@media only screen and (min-width: 481px) and (max-width: 1240px) {
-	
-	.pp { right: 0;}
-	
+
+
+	<?php wp_head(); ?>
+	<style>
+	.pp {
+		background-color: #1b7e7b;
+		border: 0 solid;
+		bottom: 0;
+		color: #fff;
+		float: right;
+		font-size: 12px;
+		font-weight: bold;
+		height: 21px;
+		margin-top: 0px;
+		padding-left: 6px;
+		padding-right: 6px;
+		position: relative;
+		right: 250px;
+	}
+
+	@media only screen and (min-width: 481px) and (max-width: 1240px) {
+
+		.pp { right: 0;}
+
 	}
 	@media only screen and (min-width: 320px) and (max-width: 480px) {
-	
-	.pp { right: 0; margin-top: 2px;}
-	
+
+		.pp { right: 0; margin-top: 2px;}
+
 	}
-</style>
+	</style>
 
 
 </head>
@@ -85,31 +85,48 @@
 
 <body <?php body_class(); ?>>
 
-<div id="page" class="hfeed site">
+	<div id="page" class="hfeed site">
 
-
-
-	<nav class="navbar navbar-default" role="navigation">
-
+<header class="top-bar">
 		<div class="container">
+			<div class="row">
+				<div class="first-info hidden-xs">
+					Designed in France, Made in the USA
+				</div>
+				<div class="pull-right">
+					<ul class="nav navbar-nav">
+						<li class="hidden-xs"><a href="http://quovadisblog.com/" target="_blank">Blog</a></li>
+						<li class="hidden-xs"><a href="/contact">Contact</a></li>
+						<li class="hidden-xs"><a href="http://eepurl.com/ca0czf">Subscribe</a></li>
+						<li class="topbook"><a href="/search/which-planner" style="border-top: 0;">Which Planner Do I Have?<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2019/07/top-book.png" style="height: 24px;margin-left: 7px;margin-top: -4px;" alt=""></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
 
-			<div class="navbar-header">
 
-			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+		<nav class="navbar navbar-default" role="navigation">
 
-			    <span class="sr-only">Toggle navigation</span>
+			<div class="container">
 
-			    <span class="icon-bar"></span>
+				<div class="navbar-header">
 
-			    <span class="icon-bar"></span>
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 
-			    <span class="icon-bar"></span>
+						<span class="sr-only">Toggle navigation</span>
 
-			  </button>
+						<span class="icon-bar"></span>
+
+						<span class="icon-bar"></span>
+
+						<span class="icon-bar"></span>
+
+					</button>
 
 
 
-				<?php if( get_header_image() != '' ) : ?>
+					<?php if( get_header_image() != '' ) : ?>
 
 
 
@@ -129,22 +146,22 @@
 
 
 
-					<div id="logo">
+				<div id="logo">
 
-						<span class="site-name"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+					<span class="site-name"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
 
-					</div><!-- end of #logo -->
-
-
-
-				<?php endif; // header image was removed (again) ?>
+				</div><!-- end of #logo -->
 
 
 
-			</div>
+			<?php endif; // header image was removed (again) ?>
 
-				<?php dazzling_header_menu(); ?>
+
 
 		</div>
+
+		<?php dazzling_header_menu(); ?>
+
+	</div>
 
 	</nav><!-- .site-navigation -->
